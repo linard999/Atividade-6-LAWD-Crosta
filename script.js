bce2170b683a19d02e2e7ac055e7454bcaa3213a
Jessica <jessicaviana.dev@gmail.com>
2024-08-26 20:33:45 -0300

    adição do modal e algumas refatorações

---
+++ script.js
@@ -0,0 +1,9 @@
+modalBackground = document.getElementsByClassName("modal-background")[0];
+
+function openModal() {
+  modalBackground.style.display = "block";
+}
+
+function closeModel() {
+  modalBackground.style.display = "none";
+}
